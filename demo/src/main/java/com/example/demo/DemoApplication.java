4c3cdfdfa5373e56b22e5b906d02e2d0497d391b
Javier <jmartin@grupoloyal.com>
2020-11-06 12:04:18 +0100

    Repositorio

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.infraestructure.repositories.ActorRepository;
 
 import lombok.Data;
 import lombok.Value;
@@ -14,9 +17,14 @@ public class DemoApplication implements CommandLineRunner {
 		SpringApplication.run(DemoApplication.class, args);		
 	}
 
+	@Autowired
+	ActorRepository dao;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola mundo.");
+		// System.out.println("Hola mundo.");
+		
+		dao.findAll().stream().forEach(a -> System.out.println(a));
 	}
 
 }
