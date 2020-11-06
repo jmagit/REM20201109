fa75699f60bfee869fce9c961d8b4465f5af4a65
Javier <jmartin@grupoloyal.com>
2020-11-06 12:42:51 +0100

    Ejemplos

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.domain.entities.Actor;
 import com.example.demo.infraestructure.repositories.ActorRepository;
 
 import lombok.Data;
@@ -21,10 +25,19 @@ public class DemoApplication implements CommandLineRunner {
 	ActorRepository dao;
 	
 	@Override
+	@Transactional
 	public void run(String... args) throws Exception {
 		// System.out.println("Hola mundo.");
 		
-		dao.findAll().stream().forEach(a -> System.out.println(a));
+		// dao.findAll().stream().forEach(a -> System.out.println(a));
+		// dao.findByFirstNameStartingWithOrderByLastNameDesc("P").stream().forEach(a -> System.out.println(a));
+		Optional<Actor> rslt = dao.findById(54);
+		if(rslt.isPresent()) {
+			Actor actor = rslt.get();
+			System.out.println(actor);
+			actor.getFilmActors().stream().forEach(o -> System.out.println(o.getFilm()));
+			// dao.findById(54).get().getFilmActors().stream().forEach(o -> System.out.println(o.getFilm()));
+		}
 	}
 
 }
--- demo/src/main/java/com/example/demo/infraestructure/repositories/ActorRepository.java
+++ demo/src/main/java/com/example/demo/infraestructure/repositories/ActorRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.infraestructure.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.domain.entities.Actor;
 
 public interface ActorRepository extends JpaRepository<Actor, Integer> {
-
+	List<Actor> findByFirstNameStartingWithOrderByLastNameDesc(String prefijo);
 }
