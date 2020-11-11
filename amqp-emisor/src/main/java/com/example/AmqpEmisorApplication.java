72a6e83d627e85bacb34ceda91b56a140e48d9f0
Javier <jmartin@grupoloyal.com>
2020-11-11 10:54:39 +0100

    Solucion

---
+++ amqp-emisor/src/main/java/com/example/AmqpEmisorApplication.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AmqpEmisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqpEmisorApplication.class, args);
+	}
+    @Bean
+    public ApplicationRunner runner(AmqpTemplate template) {
+        return args -> template.convertAndSend("saludos", "Hola mundo");
+    }
+    @Bean
+    public Queue myQueue() {
+        return new Queue("saludos");
+    }
+
+}
